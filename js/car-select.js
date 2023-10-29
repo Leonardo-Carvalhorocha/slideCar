3bf0648b224db726cb35fe7b8aa3c153b51b833f
Leonardo Carvalho <leonardo2001darocha@gmail.com>
2023-10-29 18:46:46 -0300

    effect no slidecar

--- js/car-select.js
+++ js/car-select.js
@@ -1,1 +1,14 @@
-export default function carSelect() {};
+export default function carSelect() {
+  const imgs = document.querySelectorAll('.list-car img');
+  const imgPrincipal = document.querySelector('#img-main img');
+  const events = ['touchstart', 'click'];
+  
+  imgs.forEach((img) => {
+    events.forEach((event) => img.addEventListener(event, handleSelect));
+  });
+  
+  function handleSelect({ target }) {
+    const srcImg = target.getAttribute('src');
+    imgPrincipal.setAttribute('src', srcImg);
+  }
+}
--- script.js
+++ script.js
@@ -5,9 +5,9 @@ import carSelect from './js/car-select.js';
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
+carSelect()
+
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
 slide.addControl('.custom-controls');
-
-carSelect();
